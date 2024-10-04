b07a29ca66c4a61b54b35b32dee4dab80305884c
OikawaTobi <hasanglcn18@gmail.com>
2024-10-05 01:41:08 +0300

    Bilgi oyunu

--- Bilgi Oyunu/Form1.Designer.cs
+++ Bilgi Oyunu/Form1.Designer.cs
@@ -28,12 +28,366 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            components = new System.ComponentModel.Container();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            contextMenuStrip1 = new ContextMenuStrip(components);
+            richTextBox1 = new RichTextBox();
+            label1 = new Label();
+            label2 = new Label();
+            button5 = new Button();
+            label3 = new Label();
+            label4 = new Label();
+            groupBox1 = new GroupBox();
+            label7 = new Label();
+            label8 = new Label();
+            label6 = new Label();
+            label5 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            timer1 = new System.Windows.Forms.Timer(components);
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            pictureBox5 = new PictureBox();
+            pictureBox6 = new PictureBox();
+            pictureBox7 = new PictureBox();
+            groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Franklin Gothic Medium", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            button1.Location = new Point(297, 201);
+            button1.Name = "button1";
+            button1.Size = new Size(171, 95);
+            button1.TabIndex = 0;
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Franklin Gothic Medium", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            button2.Location = new Point(527, 201);
+            button2.Name = "button2";
+            button2.Size = new Size(165, 95);
+            button2.TabIndex = 1;
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Franklin Gothic Medium", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            button3.Location = new Point(297, 327);
+            button3.Name = "button3";
+            button3.Size = new Size(171, 105);
+            button3.TabIndex = 2;
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Font = new Font("Franklin Gothic Medium", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            button4.Location = new Point(527, 327);
+            button4.Name = "button4";
+            button4.Size = new Size(154, 105);
+            button4.TabIndex = 3;
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // contextMenuStrip1
+            // 
+            contextMenuStrip1.Name = "contextMenuStrip1";
+            contextMenuStrip1.Size = new Size(61, 4);
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Font = new Font("MS UI Gothic", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            richTextBox1.Location = new Point(277, 28);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(460, 156);
+            richTextBox1.TabIndex = 5;
+            richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 14F);
+            label1.Location = new Point(102, 63);
+            label1.Name = "label1";
+            label1.Size = new Size(22, 25);
+            label1.TabIndex = 6;
+            label1.Text = "0";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 14F);
+            label2.Location = new Point(26, 63);
+            label2.Name = "label2";
+            label2.Size = new Size(64, 25);
+            label2.TabIndex = 7;
+            label2.Text = "SORU:";
+            // 
+            // button5
+            // 
+            button5.Font = new Font("AniMe Matrix - MB_EN", 15.7499981F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button5.Location = new Point(26, 141);
+            button5.Name = "button5";
+            button5.Size = new Size(171, 77);
+            button5.TabIndex = 8;
+            button5.Text = "BAŞLAT";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 14F);
+            label3.Location = new Point(60, 301);
+            label3.Name = "label3";
+            label3.Size = new Size(64, 25);
+            label3.TabIndex = 9;
+            label3.Text = "SORU:";
+            label3.Visible = false;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 14F);
+            label4.Location = new Point(60, 369);
+            label4.Name = "label4";
+            label4.Size = new Size(64, 25);
+            label4.TabIndex = 10;
+            label4.Text = "SORU:";
+            label4.Visible = false;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(label7);
+            groupBox1.Controls.Add(label8);
+            groupBox1.Controls.Add(label6);
+            groupBox1.Controls.Add(label5);
+            groupBox1.Location = new Point(888, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(255, 156);
+            groupBox1.TabIndex = 11;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "PUAN TABLOSU:";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 14F);
+            label7.Location = new Point(20, 79);
+            label7.Name = "label7";
+            label7.Size = new Size(76, 25);
+            label7.TabIndex = 15;
+            label7.Text = "YANLIŞ:";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI", 14F);
+            label8.Location = new Point(108, 79);
+            label8.Name = "label8";
+            label8.Size = new Size(22, 25);
+            label8.TabIndex = 14;
+            label8.Text = "0";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 14F);
+            label6.Location = new Point(20, 35);
+            label6.Name = "label6";
+            label6.Size = new Size(80, 25);
+            label6.TabIndex = 13;
+            label6.Text = "DOĞRU:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 14F);
+            label5.Location = new Point(108, 35);
+            label5.Name = "label5";
+            label5.Size = new Size(22, 25);
+            label5.TabIndex = 12;
+            label5.Text = "0";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Segoe UI", 14F);
+            label9.Location = new Point(830, 28);
+            label9.Name = "label9";
+            label9.Size = new Size(32, 25);
+            label9.TabIndex = 12;
+            label9.Text = "20";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Segoe UI", 14F);
+            label10.Location = new Point(743, 29);
+            label10.Name = "label10";
+            label10.Size = new Size(60, 25);
+            label10.TabIndex = 13;
+            label10.Text = "SÜRE:";
+            // 
+            // timer1
+            // 
+            timer1.Interval = 1000;
+            timer1.Tick += timer1_Tick;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.kinfe;
+            pictureBox1.Location = new Point(729, 260);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(387, 134);
+            pictureBox1.TabIndex = 14;
+            pictureBox1.TabStop = false;
+            pictureBox1.Visible = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = Properties.Resources.inheritance_ak47;
+            pictureBox2.Location = new Point(729, 260);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(299, 134);
+            pictureBox2.TabIndex = 15;
+            pictureBox2.TabStop = false;
+            pictureBox2.Visible = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = Properties.Resources.man_o_war;
+            pictureBox3.Location = new Point(729, 260);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(259, 134);
+            pictureBox3.TabIndex = 16;
+            pictureBox3.TabStop = false;
+            pictureBox3.Visible = false;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Image = Properties.Resources.anubis;
+            pictureBox4.Location = new Point(729, 260);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(226, 225);
+            pictureBox4.TabIndex = 17;
+            pictureBox4.TabStop = false;
+            pictureBox4.Visible = false;
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.Image = Properties.Resources.wonderful;
+            pictureBox5.Location = new Point(729, 260);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(226, 225);
+            pictureBox5.TabIndex = 18;
+            pictureBox5.TabStop = false;
+            pictureBox5.Visible = false;
+            // 
+            // pictureBox6
+            // 
+            pictureBox6.Image = Properties.Resources.final_logo;
+            pictureBox6.Location = new Point(729, 260);
+            pictureBox6.Name = "pictureBox6";
+            pictureBox6.Size = new Size(289, 172);
+            pictureBox6.TabIndex = 19;
+            pictureBox6.TabStop = false;
+            pictureBox6.Visible = false;
+            // 
+            // pictureBox7
+            // 
+            pictureBox7.Image = Properties.Resources.pass;
+            pictureBox7.Location = new Point(729, 260);
+            pictureBox7.Name = "pictureBox7";
+            pictureBox7.Size = new Size(259, 172);
+            pictureBox7.TabIndex = 20;
+            pictureBox7.TabStop = false;
+            pictureBox7.Visible = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.DarkRed;
+            ClientSize = new Size(1155, 574);
+            Controls.Add(pictureBox7);
+            Controls.Add(pictureBox6);
+            Controls.Add(pictureBox5);
+            Controls.Add(pictureBox4);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            Controls.Add(label10);
+            Controls.Add(label9);
+            Controls.Add(groupBox1);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(button5);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(richTextBox1);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private ContextMenuStrip contextMenuStrip1;
+        private RichTextBox richTextBox1;
+        private Label label1;
+        private Label label2;
+        private Button button5;
+        private Label label3;
+        private Label label4;
+        private GroupBox groupBox1;
+        private Label label7;
+        private Label label8;
+        private Label label6;
+        private Label label5;
+        private Label label9;
+        private Label label10;
+        private System.Windows.Forms.Timer timer1;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox5;
+        private PictureBox pictureBox6;
+        private PictureBox pictureBox7;
     }
 }
--- Bilgi Oyunu/Form1.cs
+++ Bilgi Oyunu/Form1.cs
@@ -6,5 +6,235 @@ namespace Bilgi_Oyunu
         {
             InitializeComponent();
         }
+        int soruno = 0;
+        int dogru = 0;
+        int yanlis = 0;
+        int sure = 20;
+        private void button5_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            sure = 20;
+            pictureBox1.Visible = false;
+            pictureBox2.Visible = false;
+            pictureBox3.Visible = false;
+            pictureBox4.Visible = false;
+            pictureBox5.Visible = false;
+            pictureBox6.Visible = false;
+            pictureBox7.Visible = false;
+            label9.Text=sure.ToString();
+            button5.Text = "ÝLERÝ";
+            soruno++;
+            label1.Text = soruno.ToString();
+
+            button1.Enabled = true;
+            button2.Enabled = true;
+            button3.Enabled = true;
+            button4.Enabled = true;
+
+            if (soruno == 1)
+            {
+                richTextBox1.Text = "2024 tarihinde toplam olarak en çok kill alan oyuncu kimdir ?";
+                button1.Text = "NiKo";
+                button2.Text = "jL";
+                button3.Text = "m0NESY";
+                button4.Text = "w0nderful";
+                label4.Text = "m0NESY";
+
+            }
+            if (soruno == 2)
+            {
+                richTextBox1.Text = "Faceit sýralamasýnda 1. sýrada bulunan esporcunun takýmý nedir ?";
+                button1.Text = "Natus Vincere";
+                button2.Text = "Eternal Fire";
+                button3.Text = "Team Spirit";
+                button4.Text = "Vitality";
+                label4.Text = "Team Spirit";
+            }
+            if (soruno == 3)
+            {
+                pictureBox1.Visible = true;
+                richTextBox1.Text = "Fotoðraftaki skinin adý nedir";
+                button1.Text = "Case hardened karambit";
+                button2.Text = "Bowie býçaðý ";
+                button3.Text = "Talon knife case hardened";
+                button4.Text = "Talon knife gamma doppler";
+                label4.Text = "Talon knife case hardened";
+            }
+            if (soruno == 4)
+            {
+                pictureBox2.Visible = true;
+                richTextBox1.Text = "Fotoðraftaki ak-47 skininin adý nedir ?";
+                button1.Text = "Inheritance ak-47";
+                button2.Text = "Asiimov ak-47";
+                button3.Text = "Wasteland Rebel ak-47";
+                button4.Text = "Case Hardened ak-47";
+                label4.Text = "Inheritance ak-47";
+            }
+            if (soruno == 5)
+            {
+                pictureBox3.Visible = true;
+                richTextBox1.Text = "Fotoðraftaki awp skininin adý nedir";
+                button1.Text = "Atheris awp";
+                button2.Text = "Man o war awp";
+                button3.Text = "phobos awp";
+                button4.Text = "awp fade";
+                label4.Text = "Man o war awp";
+            }
+            if (soruno == 6)
+            {
+                pictureBox4.Visible = true;
+                richTextBox1.Text = "Fotoðraftaki map hangi maptir";
+                button1.Text = "Dust 2";
+                button2.Text = "Ancient";
+                button3.Text = "Anubis";
+                button4.Text = "Ýtaly";
+                label4.Text = "Anubis";
+            }
+            if (soruno == 7)
+            {
+                pictureBox5.Visible = true;
+                richTextBox1.Text = "Fotoðraftaki esporcunun adý nedir ?";
+                button1.Text = "b1t";
+                button2.Text = "rain";
+                button3.Text = "XANTARES";
+                button4.Text = "w0nderful";
+                label4.Text = "w0nderful";
+            }
+            if (soruno == 8)
+            {
+                richTextBox1.Text = "Aktif olarak hltv sýralamasýnda en iyi cs 2 takýmý hangisidir";
+                button1.Text = "Eternal Fire";
+                button2.Text = "G2 esports";
+                button3.Text = "Vitality esports";
+                button4.Text = "Natus Vincere";
+                label4.Text = "Natus Vincere";
+            }
+            if (soruno == 9)
+            {
+                pictureBox6.Visible = true;
+                richTextBox1.Text = "Aþaðýdaki oyunculardan hangisi fotoðraftaki iki takýmda oynamamýþtýr       (Not: Aktif olarak en son maç baz alýnacaktýr.)";
+                button1.Text = "Jl";
+                button2.Text = "Xantares";
+                button3.Text = "S1mple";
+                button4.Text = "iM";
+                label4.Text = "S1mple";
+            }
+            if (soruno == 10)
+            {
+                pictureBox7.Visible = true;
+                richTextBox1.Text = "Yeni gelen güncelleme ile pass de bilet baþýna kaç yýldýz toplayabiliriz";
+                button1.Text = "200";
+                button2.Text = "45";
+                button3.Text = "40";
+                button4.Text = "150";
+                label4.Text = "40";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+
+            timer1.Enabled=false;
+
+
+            label3.Text = button1.Text;
+            if (label3.Text == label4.Text)
+            {
+                dogru++;
+                label5.Text = dogru.ToString();
+            }
+            else
+            {
+                yanlis++;
+                label8.Text = yanlis.ToString();
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+
+            timer1.Enabled = false;
+
+
+            label3.Text = button2.Text;
+            if (label3.Text == label4.Text)
+            {
+                dogru++;
+                label5.Text = dogru.ToString();
+            }
+            else
+            {
+                yanlis++;
+                label8.Text = yanlis.ToString();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+
+            timer1.Enabled = false;
+
+
+            label3.Text = button3.Text;
+            if (label3.Text == label4.Text)
+            {
+                dogru++;
+                label5.Text = dogru.ToString();
+            }
+            else
+            {
+                yanlis++;
+                label8.Text = yanlis.ToString();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+
+            timer1.Enabled = false;
+
+
+            label3.Text = button4.Text;
+            if (label3.Text == label4.Text)
+            {
+                dogru++;
+                label5.Text = dogru.ToString();
+            }
+            else
+            {
+                yanlis++;
+                label8.Text = yanlis.ToString();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            sure=Convert.ToInt32(label9.Text);
+            sure = sure - 1;
+            label9.Text = sure.ToString();
+            if (sure == 0)
+            {
+                timer1.Enabled = false;
+                yanlis++;
+                label8.Text = yanlis.ToString();
+            }
+        }
     }
 }
